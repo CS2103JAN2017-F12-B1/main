5605cf2d90ba8a7ab3a2c4bb02c91a7c16889340
coyotestarrkwsq <espada_wsq@hotmail.com>
2017-03-30 19:21:52 +0800

    undo works with mark or unmark now

---
+++ src/main/java/savvytodo/model/operations/RedoMarkOperation.java
@@ -0,0 +1,53 @@
+package savvytodo.model.operations;
+
+import java.util.EmptyStackException;
+
+import savvytodo.logic.commands.exceptions.CommandException;
+
+//author @@A0124863A
+/**
+* @author A0124863A
+* Redo a mark operation by restoring the marked or unmarked task to its original
+*/
+public class RedoMarkOperation extends Operation {
+    private int numToUnmark;
+    private UndoRedoOperationCentre undoRedoOpCentre;
+
+
+    public RedoMarkOperation(int numToUnmark) {
+        this.numToUnmark = numToUnmark;
+        this.setTaskManager(taskManager);
+    }
+
+    public void setUndoRedoOperationCentre(UndoRedoOperationCentre undoRedoOpCentre) {
+        this.undoRedoOpCentre = undoRedoOpCentre;
+    }
+
+    @Override
+    public void execute() throws CommandException {
+        assert taskManager != null;
+        assert undoRedoOpCentre != null;
+
+        try {
+            for (int i = 0; i < numToUnmark; i++) {
+                Operation redo = undoRedoOpCentre.getRedoOperation();
+                assert redo.getClass().isAssignableFrom(UndoEditOperation.class) == true;
+                redo.setTaskManager(taskManager);
+                redo.execute();
+            }
+        } catch (EmptyStackException e) {
+            throw new CommandException(e.getMessage());
+        } catch (CommandException e) {
+            throw new CommandException(e.getMessage());
+        }
+
+        undoRedoOpCentre.storeUndoOperation(this.reverse());
+
+    }
+
+    @Override
+    public Operation reverse() {
+        return new UndoMarkOperation(numToUnmark);
+    }
+
+}
