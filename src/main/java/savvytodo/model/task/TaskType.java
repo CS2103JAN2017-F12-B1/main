45f28cb5bf8e317eca58f85a3f600dd0c23372d2
Joel Lim <103879u@gmail.com>
2017-04-09 14:21:32 +0800

    resolve merge conflict

---
+++ src/main/java/savvytodo/model/task/TaskType.java
@@ -0,0 +1,11 @@
+package savvytodo.model.task;
+
+//@@author A0147828U
+/**
+ * Enum to differentiate between the types of tasks
+ * @author jingloon
+ *
+ */
+public enum TaskType {
+    FLOATING, EVENT, DEADLINE
+}
