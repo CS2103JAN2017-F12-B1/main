ae310a3b416a6d89cf9c58ddbceb7f165b0abe85
Joel Lim <103879u@gmail.com>
2017-04-09 14:16:00 +0800

    resolve conflict

--- src/main/java/savvytodo/model/task/Task.java
+++ src/main/java/savvytodo/model/task/Task.java
@@ -1,8 +1,5 @@
 package savvytodo.model.task;
 
-import java.util.Arrays;
-import java.util.Objects;
-
 import savvytodo.commons.util.CollectionUtil;
 import savvytodo.model.category.UniqueCategoryList;
 
@@ -21,6 +18,8 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     private DateTime dateTime;
     private Recurrence recurrence;
     private Status isCompleted;
+    private TimeStamp timeStamp;
+    private Type type;
 
     private UniqueCategoryList categories;
 
@@ -39,11 +38,29 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
         this.dateTime = dateTime;
         this.recurrence = recurrence;
         this.isCompleted = new Status();
+        this.type = new Type(dateTime);
+        this.timeStamp = new TimeStamp();
     }
 
     public Task(Name name, Priority priority, Description description, Location location,
             UniqueCategoryList categories, DateTime dateTime, Recurrence recurrence,
-            Status status) {
+            TimeStamp timeStamp) {
+        assert !CollectionUtil.isAnyNull(name, priority, description, location, categories,
+                dateTime, recurrence, timeStamp);
+        this.name = name;
+        this.priority = priority;
+        this.description = description;
+        this.location = location;
+        this.categories = new UniqueCategoryList(categories); //protect internal categories from changes in the arg list
+        this.dateTime = dateTime;
+        this.recurrence = recurrence;
+        this.isCompleted = new Status();
+        this.timeStamp = new TimeStamp(timeStamp);
+    }
+
+    public Task(Name name, Priority priority, Description description, Location location,
+            UniqueCategoryList categories, DateTime dateTime, Recurrence recurrence, Status status,
+            TimeStamp timeStamp) {
         assert !CollectionUtil.isAnyNull(name, priority, description, location, categories,
                 dateTime, recurrence, status);
         this.name = name;
@@ -54,6 +71,8 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
         this.dateTime = dateTime;
         this.recurrence = recurrence;
         this.isCompleted = status;
+        this.type = new Type(dateTime);
+        this.timeStamp = new TimeStamp(timeStamp);
     }
 
     /**
@@ -62,7 +81,7 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     public Task(ReadOnlyTask source) {
         this(source.getName(), source.getPriority(), source.getDescription(), source.getLocation(),
                 source.getCategories(), source.getDateTime(), source.getRecurrence(),
-                source.isCompleted());
+                source.isCompleted(), source.getTimeStamp());
     }
 
     public void setName(Name name) {
@@ -161,6 +180,8 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
         this.setDateTime(replacement.getDateTime());
         this.setRecurrence(replacement.getRecurrence());
         this.setStatus(replacement.isCompleted());
+        this.setType(replacement.getType());
+        this.setTimeStamp(replacement.getTimeStamp());
     }
 
     @Override
@@ -173,12 +194,56 @@ public class Task implements ReadOnlyTask, Comparable<Task> {
     @Override
     public int hashCode() {
         // use this method for custom fields hashing instead of implementing your own
-        return Objects.hash((Object[]) attributes());
+        return Objects.hash(name, priority, description, location, categories, dateTime, recurrence,
+                isCompleted, timeStamp);
     }
 
     @Override
     public String toString() {
         return getAsText();
+    }
+
+    //@@author A0147827U
+    private boolean isFloating() {
+        return getDateTime().getStartDate() == null && getDateTime().getEndDate() == null;
+    }
+
+    private boolean isEvent() {
+        return !(getDateTime().getStartDate() == null && getDateTime().getEndDate() == null);
+    }
+
+    private boolean isDeadline() {
+        return getDateTime().getStartDate() == null && !(getDateTime().getEndDate() == null);
+    }
+
+    public Type getType() {
+        updateType();
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    private void updateType() {
+        if (isEvent()) {
+            type.setType(TaskType.EVENT);
+        } else if (isFloating()) {
+            type.setType(TaskType.FLOATING);
+        } else if (isDeadline()) {
+            type.setType(TaskType.DEADLINE);
+        }
+    }
+
+    //@@author A0124863A
+    @Override
+    public TimeStamp getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(TimeStamp timeStamp) {
+        assert timeStamp != null;
+        this.timeStamp = timeStamp;
     }
 
     //@@author A0140036X
